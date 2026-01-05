42ba196af420dc0da86a475d3489e5ee47741799
Arta Tri Pamuda <pamuda.id@gmail.com>
2026-01-05 11:04:51 +0700

    ganti metode env -> postgresql

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config(); // ✅ load .env duluan
+
 import { Pool } from "pg";
 
 export const pg = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: String(process.env.DATABASE_PASSWORD), // ⬅️ aman
+  database: process.env.DATABASE_NAME,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+  options: '-c search_path=public'
 });
