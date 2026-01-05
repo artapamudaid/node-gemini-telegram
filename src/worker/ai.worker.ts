815f25eb56a7e4295e294b89ade47fec4095d829
Arta Tri Pamuda <pamuda.id@gmail.com>
2026-01-05 11:39:50 +0700

    hapus log postgre config

--- src/worker/ai.worker.ts
+++ src/worker/ai.worker.ts
@@ -94,13 +94,6 @@ new Worker(
     // ===============================
     // 🔹 POSTGRES LOG
     // ===============================
-    console.log("[PG CONNECT]", {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-    });
 
 
     try {
